07a033dfbaff781141076d550a0f6c7ad749bfec
FrancoGroenewald <franco@softwareworx.co.za>
2023-06-27 15:07:06 +0000

    create components

--- runninghill/src/app/app.module.ts
+++ runninghill/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentsComponent } from './components/components.component';
+import { HeadersComponent } from './components/headers/headers.component';
+import { SentencesComponent } from './sentences/sentences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HeadersComponent,
+    SentencesComponent
   ],
   imports: [
     BrowserModule,
