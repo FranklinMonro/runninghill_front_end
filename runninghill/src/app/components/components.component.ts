e3f8d3c5a3c409897cdb33b13a077243eb4bee42
FrancoGroenewald <franco@softwareworx.co.za>
2023-06-27 15:23:44 +0000

    fix eslint parse

--- runninghill/src/app/components/components.component.ts
+++ runninghill/src/app/components/components.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-components',
   templateUrl: './components.component.html',
-  styleUrls: ['./components.component.scss']
+  styleUrls: ['./components.component.scss'],
 })
 export class ComponentsComponent {
 
