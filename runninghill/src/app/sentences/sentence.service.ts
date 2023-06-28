2e6aab3c2423d83ec7d9c760729f6dd7c6102f62
FranklinMonro <fbg2468@gmail.com>
2023-06-28 14:50:14 +0000

    sentence service

--- runninghill/src/app/sentences/sentence.service.ts
+++ runninghill/src/app/sentences/sentence.service.ts
@@ -1,9 +1,29 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/prefer-default-export */
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import environment from '../../enviroment/enviroment';
+import { WordTypes } from './sentences.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SentenceService {
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  public getUsers = (): Observable<WordTypes> => this.httpClient.get<WordTypes>(
+    `${environment.apiUrl}runninghillapi/sentenceroutes/wordtype`,
+    { observe: 'response' },
+  ).pipe(
+    map((res: any) => {
+      if (!res.body.response) {
+        throw new Error('No response in body');
+      }
+      return res;
+    }),
+    catchError((err: HttpErrorResponse) => { throw new Error(err.message); }),
+  );
 }
---
+++ runninghill/src/app/sentences/sentences.interface.ts
@@ -0,0 +1,14 @@
+interface WordTypes {
+  typeID: string;
+  wordType?: string;
+}
+
+interface SentenceTypes {
+  senteceID: string;
+  sentence?: string;
+}
+
+export {
+  WordTypes,
+  SentenceTypes,
+};
