66d70f78e7f1d9aa2d567de7debc0ac05829e6fb
FranklinMonro <fbg2468@gmail.com>
2023-06-28 15:11:48 +0000

    path route

--- runninghill/src/app/app-routing.module.ts
+++ runninghill/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+/* eslint-disable import/prefer-default-export */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SentencesComponent } from './sentences/sentences.component';
+
+const routes: Routes = [
+  { path: 'sentence', component: SentencesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- runninghill/src/app/app.module.ts
+++ runninghill/src/app/app.module.ts
@@ -1,28 +1,28 @@
+/* eslint-disable import/prefer-default-export */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentsComponent } from './components/components.component';
 import { HeadersComponent } from './components/headers/headers.component';
 import { SentencesComponent } from './sentences/sentences.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentsComponent,
     HeadersComponent,
-    SentencesComponent
+    SentencesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgbModule
+    NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
